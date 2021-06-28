de425901e5f862c95a7715ac0992c4b9dcd4f94f
steeltx <realsteel.gnu@gmail.com>
2021-06-27 20:46:08 -0500

    Inicializar vue y vincular propiedades de input

---
+++ app.js
@@ -0,0 +1,10 @@
+Vue.createApp({
+    data(){
+        return{
+            perspective: 100,
+            rotateX: 0,
+            rotateY: 0,
+            rotateZ: 0
+        }
+    }
+}).mount('#app')
